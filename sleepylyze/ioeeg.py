7d569668f0048a95a34019e6bd874728ea59ba54
nrecoder <natasha.recoder@gmail.com>
2022-04-11 17:20:04 -0400

    changed encoding

--- sleepylyze/ioeeg.py
+++ sleepylyze/ioeeg.py
@@ -137,12 +137,12 @@ class Dataset:
         print("Patient identifier:", self.metadata['in_num'])
 
         # read the first line to check encoding
-        with open(self.metadata['filepath'], 'r') as f:
+        with open(self.metadata['filepath'], encoding='utf16') as f:
             line = f.readline()
         if line[3] == '\x00':
             f_encoding = 'utf-16-le'
         else:
-            f_encoding = 'ascii' #might want to set this to None (used with open and pd.read_csv)
+            f_encoding = 'utf16' #might want to set this to None (used with open and pd.read_csv)
         self.metadata['encoding'] = f_encoding
         
         # extract sampling freq, # of channels, headbox sn
