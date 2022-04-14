07933f08d55eed7aca27f860daf09b1d24fd1639
nrecoder <natasha.recoder@gmail.com>
2022-04-14 14:52:04 -0400

    set value depricated

--- sleepylyze/ioeeg.py
+++ sleepylyze/ioeeg.py
@@ -504,12 +504,12 @@ class Dataset:
                     if cx == '*':
                         for x in eeg_channels:
                             #self.data[(x, 'Raw')].loc[t] = np.NaN # much slower
-                            #self.data.at[t, (x, 'Raw')] = np.NaN # not depricated, slightly slower
-                            self.data.set_value(t, (x, 'Raw'), np.NaN)
+                            self.data.at[t, (x, 'Raw')] = np.NaN # not depricated, slightly slower
+                            #self.data.set_value(t, (x, 'Raw'), np.NaN)
                     else:
                         #self.data[(cx, 'Raw')].loc[t] = np.NaN
-                        #self.data.at[t, (cx, 'Raw')] = np.NaN
-                        self.data.set_value(t, (cx, 'Raw'), np.NaN)
+                        self.data.at[t, (cx, 'Raw')] = np.NaN
+                        #self.data.set_value(t, (cx, 'Raw'), np.NaN)
 
         print('Data cleaned.')
 
